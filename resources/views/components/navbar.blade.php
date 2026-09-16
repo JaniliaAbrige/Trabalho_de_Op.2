314d2cc667825e15ea5eaef1a8b131866aad592a
Janny <janiliaabrige@gmail.com>
2026-09-16 09:05:27 +0200

    Atualiza navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -101,6 +101,7 @@
 
                     <ul class="dropdown-menu">
 
+<<<<<<< Updated upstream
                         <li>
                             <a class="dropdown-item"
                                href="{{ Route::has('inscricoes.index') ? route('inscricoes.index') : '#' }}">
@@ -303,6 +304,9 @@
                 @endauth
 
             </div>
+=======
+        <a href="{{ url('/') }}" class="navbar-logo">JAM-acad<span>.</span></a>
+>>>>>>> Stashed changes
 
         </div>
     </div>
